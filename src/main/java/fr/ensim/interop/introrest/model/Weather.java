1b6b948c6e6a31634679c01e942e35e9d5b08ea0
Mateo.O <mateo.olivier72@gmail.com>
2026-04-13 16:45:56 +0200

    Complété Weather

--- src/main/java/fr/ensim/interop/introrest/model/Weather.java
+++ src/main/java/fr/ensim/interop/introrest/model/Weather.java
@@ -1,4 +1,22 @@
 package fr.ensim.interop.introrest.model;
 
 public class Weather {
+    private Integer id;
+    private Main main;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
 }
